4f8226882f5e8a6a754aab0792a7fceb6ea9136d
Suresh Ghasal <152689946+Hyysuresh@users.noreply.github.com>
2026-02-10 19:16:09 +0530

    Update default Docker credentials name

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -10,7 +10,7 @@
 def call(Map config = [:]) {
     def imageName = config.imageName ?: error("Image name is required")
     def imageTag = config.imageTag ?: 'latest'
-    def credentials = config.credentials ?: 'docker-hub-credentials'
+    def credentials = config.credentials ?: 'DockerHubCreds'
     
     echo "Pushing Docker image: ${imageName}:${imageTag}"
     
