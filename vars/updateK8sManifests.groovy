023f299a600b1a489794e60f2e0ac46e27cca8a6
Suresh Ghasal <152689946+Hyysuresh@users.noreply.github.com>
2026-02-15 09:17:21 +0530

    Modify git add to include all files in commit
    Change git add command to add all files instead of specific YAML files.

--- vars/updateK8sManifests.groovy
+++ vars/updateK8sManifests.groovy
@@ -47,7 +47,7 @@ def call(Map config = [:]) {
                 echo "No changes to commit"
             else
                 # Commit and push changes
-                git add ${manifestsPath}/*.yaml
+                git add .
                 git commit -m "Update image tags to ${imageTag} and ensure correct domain [ci skip]"
                 
                 # Set up credentials for push
